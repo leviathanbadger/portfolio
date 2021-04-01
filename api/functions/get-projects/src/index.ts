7342599375f7a15b3e0120e0f549091446c8e0c5
Brandon Slade <brandonyoyoslade@gmail.com>
2021-04-01 13:42:29 -0500

    feat(api): add get-projects lambda function

---
+++ api/functions/get-projects/src/index.ts
@@ -0,0 +1,34 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2 } from 'aws-lambda';
+import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
+
+export async function lambdaHandler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> {
+  const ddb = new DynamoDBClient({
+    apiVersion: '2012-08-10',
+    region: 'us-east-1'
+  });
+
+  const command = new ScanCommand({
+    TableName: 'PortfolioProjects'
+  });
+
+  try {
+    const results = await ddb.send(command);
+    console.info(`Scanned table. There were ${results.Items!.length} items returned of ${results.Count}`);
+    const items = results.Items!.map(item => unmarshall(item));
+    return {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(items)
+    };
+  }
+  catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err)
+    };
+  }
+}
