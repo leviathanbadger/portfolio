486a6d350000028fa5461e4089c9fffd668f27b3
Brandon Slade <brandonyoyoslade@gmail.com>
2022-10-19 23:08:26 -0600

    feat(frontend): add link to view resume as PDF

--- frontend/src/modules/+resume/pages/resume/resume.component.ts
+++ frontend/src/modules/+resume/pages/resume/resume.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss']
 })
-export class ResumeComponent { }
+export class ResumeComponent {
+  faFilePdf = faFilePdf;
+}
--- frontend/src/modules/+resume/resume.module.ts
+++ frontend/src/modules/+resume/resume.module.ts
@@ -6,10 +6,11 @@ import { ResumeComponent } from './pages/resume/resume.component';
 //Imports
 import { RoutingModule } from './routing-module';
 import { SharedModule } from 'src/shared/shared.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [ResumeComponent],
-  imports: [RoutingModule, SharedModule],
+  imports: [RoutingModule, SharedModule, FontAwesomeModule],
   exports: [ResumeComponent]
 })
 export class ResumeModule { }
