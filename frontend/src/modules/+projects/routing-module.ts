887d1aabb56058315374a3916620212d7fa4dded
Brandon Slade <brandonyoyoslade@gmail.com>
2023-01-06 22:20:26 -0700

    feat(frontend): add route to navigate to current project from sitemap

--- frontend/src/modules/+projects/routing-module.ts
+++ frontend/src/modules/+projects/routing-module.ts
@@ -6,6 +6,7 @@ import { ProjectIndexComponent } from './pages/project-index/project-index.compo
 export const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'index', data: {routeType: 'tile-list'}},
   {path: 'index', component: ProjectIndexComponent, data: {routeType: 'tile-list'}},
+  {path: 'current', pathMatch: 'full', redirectTo: 'vulkan-rust-game-engine', data: {routeType: 'tile'}},
   {path: ':projectSlug', component: ProjectComponent, data: {routeType: 'tile'}}
 ];
 
