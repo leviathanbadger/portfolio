7790cf525c62d85873850205bf92c890715d381c
Brandon Slade <brandonyoyoslade@gmail.com>
2023-01-07 20:09:34 -0700

    feat(frontend): add enter animations for homepage and resume

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faStackOverflow, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('blowup', [
+      transition(':enter', [
+        style({ 'scale': 0 }),
+        animate('.4s cubic-bezier(0.175,  0.885, 0.320, 1.275)', style({ 'scale': 1 }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent {
   faArrowRight = faArrowRight;
--- frontend/src/modules/+resume/pages/resume/resume.component.ts
+++ frontend/src/modules/+resume/pages/resume/resume.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.scss']
+  styleUrls: ['./resume.component.scss'],
+  animations: [
+    trigger('staggerpopin_sections', [
+      transition(':enter', [
+        query('.section, .subsection, .certification-tile', [
+          style({ 'scale': 0 }),
+          stagger('.05s', [
+            animate('.4s cubic-bezier(0.175,  0.885, 0.320, 1.275)', style({ 'scale': 1 })),
+          ]),
+        ])
+      ])
+    ])
+  ]
 })
 export class ResumeComponent {
   faFilePdf = faFilePdf;
