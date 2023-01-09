d4ef049174e7e422d86af55b07ea609dc9b2b37f
Brandon Slade <brandonyoyoslade@gmail.com>
2023-01-08 18:34:50 -0700

    feat(frontend): keep previous timeline event on current screen while loading next

--- frontend/src/modules/+projects/pages/vulkan-rust-timeline-entry/vulkan-rust-timeline-entry.component.ts
+++ frontend/src/modules/+projects/pages/vulkan-rust-timeline-entry/vulkan-rust-timeline-entry.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, filter, switchMap, distinctUntilChanged, shareReplay } from 'rxjs/operators';
+import { map, filter, switchMap, distinctUntilChanged, shareReplay, debounceTime } from 'rxjs/operators';
 import { SlideshowItem } from 'src/shared/components/slideshow/slideshow.component';
 import { ProjectService } from 'src/shared/services/project.service';
 import { RustEngineTimelineEntry } from 'src/shared/models/rust-engine-timeline-entry';
-import { isResultResolved, Result } from 'src/shared/models/result';
+import { isResultDoneLoading, isResultResolved, Result } from 'src/shared/models/result';
 
 @Component({
   templateUrl: './vulkan-rust-timeline-entry.component.html',
@@ -13,6 +13,9 @@ import { isResultResolved, Result } from 'src/shared/models/result';
 })
 export class VulkanRustTimelineEntryComponent {
   entryResult$!: Observable<Result<RustEngineTimelineEntry>>;
+  entry$!: Observable<RustEngineTimelineEntry>;
+  isLoading$!: Observable<boolean>;
+  error$!: Observable<string | null>;
 
   slideshowItems$!: Observable<SlideshowItem[]>;
 
@@ -29,9 +32,24 @@ export class VulkanRustTimelineEntryComponent {
       shareReplay(1)
     );
 
-    this.slideshowItems$ = this.entryResult$.pipe(
+    this.entry$ = this.entryResult$.pipe(
       filter(isResultResolved),
-      map(practice_r => practice_r.result),
+      map(events_r => events_r.result! || []),
+      debounceTime(0)
+    );
+
+    this.error$ = this.entryResult$.pipe(
+      filter(isResultDoneLoading),
+      map(events_r => events_r.isError ? events_r.error : null),
+      distinctUntilChanged()
+    );
+
+    this.isLoading$ = this.entryResult$.pipe(
+      map(events_r => events_r.isLoading),
+      distinctUntilChanged()
+    );
+
+    this.slideshowItems$ = this.entry$.pipe(
       map(practice => {
         if (!practice) return [];
         return practice.assets;
