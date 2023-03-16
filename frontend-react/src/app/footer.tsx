52501aba0c12f4798ac9382bbdde3b4e1fabcbea
Brandon Slade <brandonyoyoslade@gmail.com>
2023-03-15 22:15:34 -0600

    feat(frontend-react): add github, stack overflow, and linked in icons to footer

--- frontend-react/src/app/footer.tsx
+++ frontend-react/src/app/footer.tsx
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
+import { faGithub, faStackOverflow, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './footer.scss';
 
 function Footer() {
@@ -17,6 +19,15 @@ function Footer() {
                     </div>
 
                     <ul className="nav profile-icons col-4 justify-content-end">
+                        <li className='nav-item'>
+                            <Link href="https://github.com/leviathanbadger" className="px-2 text-light"><FontAwesomeIcon icon={faGithub} size="2x" /></Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link href="https://stackoverflow.com/users/768597/leviathanbadger" className="px-2 text-light"><FontAwesomeIcon icon={faStackOverflow} size="2x" /></Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link href="https://www.linkedin.com/in/brandon-slade" className="px-2 text-light"><FontAwesomeIcon icon={faLinkedin} size="2x" /></Link>
+                        </li>
                     </ul>
                 </div>
             </div>
