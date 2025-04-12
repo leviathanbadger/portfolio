37d515c77eb37d9d5e3d7230531de3b9d2dee6a4
Brandon Slade <brandonyoyoslade@gmail.com>
2025-04-11 23:32:27 -0600

    feat(portfolio-ssr): add ng-icons package and include icons for footer links to external sites

--- portfolio-ssr/src/app/layouts/default-layout/components/footer/footer.component.ts
+++ portfolio-ssr/src/app/layouts/default-layout/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { faBrandGithub, faBrandLinkedin, faBrandStackOverflow } from '@ng-icons/font-awesome/brands';
 
 @Component({
     selector: 'bls-footer',
-    imports: [RouterLink, RouterLinkActive],
+    imports: [RouterLink, RouterLinkActive, NgIcon],
     templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss'
+    styleUrl: './footer.component.scss',
+    viewProviders: [provideIcons({ faBrandGithub, faBrandStackOverflow, faBrandLinkedin })]
 })
 export class FooterComponent {
     readonly currentYear: number = new Date().getFullYear();
